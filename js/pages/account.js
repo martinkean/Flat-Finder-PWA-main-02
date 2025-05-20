9b400a16829d063d26345b1a532f1934013ca54c
Martin Kean <martinkean15@gmail.com>
2025-05-20 14:59:39 +1200

    Update info button styling

--- js/pages/account.js
+++ js/pages/account.js
@@ -413,7 +413,7 @@ function loadCurrentFlatTab(container, userProfile) {
         </div>
       </div>
       <div class="property-actions">
-        <button class="action-button info" aria-label="More information">
+        <button class="action-button info" aria-label="More information" onclick="showPropertyDetails(${property.id})">
           <svg viewBox="0 0 24 24">
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
           </svg>
@@ -552,8 +552,9 @@ function loadCurrentFlatTab(container, userProfile) {
   const shareButton = content.querySelector('.action-button.share');
 
   if (infoButton) {
-    infoButton.addEventListener('click', () => {
-      showToast('Flat details coming soon!');
+    infoButton.addEventListener('click', (e) => {
+      e.stopPropagation();
+      showPropertyDetails(userProfile.documents.flattingHistory[0].id);
     });
   }
 
