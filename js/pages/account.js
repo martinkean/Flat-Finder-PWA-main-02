f1437c8ca5d786da4e4f4b36bccf9083b4bcc214
Martin Kean <martinkean15@gmail.com>
2025-05-20 15:05:34 +1200

    Fix undefined property reference

--- js/pages/account.js
+++ js/pages/account.js
@@ -413,7 +413,7 @@ function loadCurrentFlatTab(container, userProfile) {
         </div>
       </div>
       <div class="property-actions">
-        <button class="action-button info" aria-label="More information" onclick="showPropertyDetails(${property.id})">
+        <button class="action-button info" aria-label="More information">
           <svg viewBox="0 0 24 24">
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
           </svg>
@@ -554,7 +554,14 @@ function loadCurrentFlatTab(container, userProfile) {
   if (infoButton) {
     infoButton.addEventListener('click', (e) => {
       e.stopPropagation();
-      showPropertyDetails(userProfile.documents.flattingHistory[0].id);
+      const propertyId = userProfile.documents.flattingHistory && userProfile.documents.flattingHistory.length > 0 
+        ? userProfile.documents.flattingHistory[0].id 
+        : null;
+      if (propertyId) {
+        showPropertyDetails(propertyId);
+      } else {
+        showToast('Property details not available');
+      }
     });
   }
 
